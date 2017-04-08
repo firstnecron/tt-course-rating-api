61332f287e8e40bb7b07e56e3b0374c847de0e82
Justin Russo <j.russo@outlook.com>
2017-04-08 17:32:10 -0400

    Linting

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
 'use strict';
 
-// load modules
-var express = require('express');
-var morgan = require('morgan');
+// Load modules
+const express = require('express');
+const morgan = require('morgan');
 
-var app = express();
+const app = express();
 
-// set our port
+// Set our port
 app.set('port', process.env.PORT || 5000);
 
-// morgan gives us http request logging
+// Morgan gives us http request logging
 app.use(morgan('dev'));
 
-// setup our static route to serve files from the "public" folder
+// Setup our static route to serve files from the "public" folder
 app.use('/', express.static('public'));
 
-// catch 404 and forward to global error handler
-app.use(function(req, res, next) {
-  var err = new Error('File Not Found');
-  err.status = 404;
-  next(err);
+// Catch 404 and forward to global error handler
+app.use((req, res, next) => {
+	const err = new Error('File Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // Express's global error handler
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
-// start listening on our port
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server is listening on port ' + server.address().port);
+// Start listening on our port
+const server = app.listen(app.get('port'), () => {
+	console.log('Express server is listening on port ' + server.address().port);
 });
