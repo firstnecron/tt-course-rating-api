248b69a3d1f9fd68276b7ef54816779bb5675d19
Justin Russo <j.russo@outlook.com>
2017-04-09 13:16:52 -0400

    Fix error response

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -5,24 +5,26 @@ const User = require('../models').User;
 function authenticate(req, res, next) {
 	const credentials = basicAuth(req);
 
-	User.authenticate(credentials.name, credentials.pass, (error, user) => {
-		if (error) {
-			return next(error);
-		}
+	if (credentials) {
+		User.authenticate(credentials.name, credentials.pass, (error, user) => {
+			if (error) {
+				return next(error);
+			}
 
-		if (!user) {
-			const error = new Error('Credentials do not match');
-			error.status = 404;
-			return next(error);
-		}
+			if (!user) {
+				const error = new Error('Credentials do not match');
+				error.status = 404;
+				return next(error);
+			}
 
-		req.user = user;
-		return next();
-	});
-
-	const error = new Error('You must be logged in to perform this action.');
-	error.status = 401;
-	return next(error);
+			req.user = user;
+			return next();
+		});
+	} else {
+		const error = new Error('You must be logged in to perform this action.');
+		error.status = 401;
+		return next(error);
+	}
 }
 
 module.exports = {
