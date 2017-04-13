e545c93dd7f1bc67c668d8661f54effed7e6515f
Justin Russo <j.russo@outlook.com>
2017-04-13 19:33:21 -0400

    Send entire error in error handler

--- src/index.js
+++ src/index.js
@@ -58,12 +58,7 @@ app.use((req, res, next) => {
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
 	res.status(err.status || 500);
-	res.json({
-		error: {
-			message: err.message,
-			error: {}
-		}
-	});
+	res.json(err);
 });
 
 // Start listening on our port
